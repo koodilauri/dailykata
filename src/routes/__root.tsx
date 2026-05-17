160423483e97a7810ee176b91262153523bb1fab
Lauri <lakoivisto@gmail.com>
2026-05-17 23:10:56 +0300

    update favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,7 +46,13 @@ export const Route = createRootRoute({
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { title: 'dailykata' }
     ],
-    links: [{ rel: 'stylesheet', href: appCss }]
+    links: [
+      { rel: 'stylesheet', href: appCss },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon_io/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon_io/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon_io/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon_io/apple-touch-icon.png' },
+    ]
   }),
   shellComponent: RootDocument,
   component: RootLayout
